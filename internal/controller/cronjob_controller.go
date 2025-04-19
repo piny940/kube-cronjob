fb025bd75f53de867d4ee3cd60026714c8445898
piny940 <83708535+piny940@users.noreply.github.com>
2025-04-19 15:44:16 +0900

    cronjobのchild jobsをactive, successful, failedに分類
    Signed-off-by: piny940 <83708535+piny940@users.noreply.github.com>

--- internal/controller/cronjob_controller.go
+++ internal/controller/cronjob_controller.go
@@ -25,6 +25,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	appv1alpha1 "github.com/piny940/kube-cronjob/api/v1alpha1"
+	batchv1 "k8s.io/api/batch/v1"
 )
 
 // CronJobReconciler reconciles a CronJob object
@@ -50,9 +51,46 @@ type CronJobReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// cronjob := r.Client.
+	var cronjob appv1alpha1.CronJob
+	if err := r.Client.Get(ctx, req.NamespacedName, &cronjob); err != nil {
+		log.Error(err, "failed to get cronjob")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	var jobs batchv1.JobList
+	if err := r.Client.List(ctx, &jobs, client.InNamespace(req.Namespace), client.MatchingFields{
+		"jobOwnerKey": req.Name,
+	}); err != nil {
+		log.Error(err, "failed to list child jobs", "cronjob", req.Name)
+		return ctrl.Result{}, err
+	}
+
+	var activeJobs []*batchv1.Job
+	var successfulJobs []*batchv1.Job
+	var failedJobs []*batchv1.Job
+
+	isJobFinished := func(job *batchv1.Job) (bool, batchv1.JobConditionType) {
+		for _, t := range job.Status.Conditions {
+			if t.Type == batchv1.JobComplete || t.Type == batchv1.JobFailed {
+				return true, t.Type
+			}
+		}
+		return false, ""
+	}
+
+	for _, job := range jobs.Items {
+		_, finishedType := isJobFinished(&job)
+		switch finishedType {
+		case batchv1.JobComplete:
+			successfulJobs = append(successfulJobs, &job)
+		case batchv1.JobFailed:
+			failedJobs = append(failedJobs, &job)
+		default:
+			activeJobs = append(activeJobs, &job)
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
